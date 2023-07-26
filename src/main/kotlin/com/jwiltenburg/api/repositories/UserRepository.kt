003055037a90d0ec81f4e22b44c1e1825ff9e8be
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-07-26 16:44:27 -0300

    Creating repository layer

---
+++ src/main/kotlin/com/jwiltenburg/api/repositories/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.jwiltenburg.api.repositories
+
+import com.jwiltenburg.api.entities.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<UserEntity, Long> {
+}
