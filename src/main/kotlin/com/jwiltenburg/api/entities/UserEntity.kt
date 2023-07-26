5612b5837e5008903629d3091f497b1325bbc2f5
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-07-26 16:09:47 -0300

    Refactoring with best practices

--- src/main/kotlin/com/jwiltenburg/api/entities/UserEntity.kt
+++ src/main/kotlin/com/jwiltenburg/api/entities/UserEntity.kt
@@ -8,8 +8,16 @@ data class UserEntity (
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long,
+
+        @Column(name = "name_user", nullable = false)
         val name: String,
+
+        @Column(name = "email_user", nullable = false)
         val email: String,
+
+        @Column(name = "date_of_birth_user", nullable = false)
         val dateOfBirth: LocalDate,
+
+        @Column(name = "age_user", nullable = false)
         val age: Integer,
 )
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
-INSERT INTO tb_user (name, email, date_of_birth, age) VALUES ('Jefferson', 'jefferson@email.com', '1987-04-04', 36);
-INSERT INTO tb_user (name, email, date_of_birth, age) VALUES ('Julio', 'julio@email.com', '2010-07-22', 13);
+INSERT INTO tb_user (name_user, email_user, date_of_birth_user, age_user) VALUES ('Jefferson', 'jefferson@email.com', '1987-04-04', 36);
+INSERT INTO tb_user (name_user, email_user, date_of_birth_user, age_user) VALUES ('Julio', 'julio@email.com', '2010-07-22', 13);
+INSERT INTO tb_user (name_user, email_user, date_of_birth_user, age_user) VALUES ('Mariana', 'mariana@email.com', '2013-02-04', 10);
+INSERT INTO tb_user (name_user, email_user, date_of_birth_user, age_user) VALUES ('Gustavo', 'gustavo@email.com', '2014-03-04', 9);
