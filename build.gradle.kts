1752debb718d5339349a330105a4a42ba76b62d2
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-07-27 16:01:03 -0300

    Adding Spring Validator dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
