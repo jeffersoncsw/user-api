f734642eab7b7082568de69b5e66de107fc609a2
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-07-27 13:57:45 -0300

    Service refactored to throw custom exceptions

--- src/main/kotlin/com/jwiltenburg/api/services/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/jwiltenburg/api/services/impl/UserServiceImpl.kt
@@ -4,6 +4,7 @@ import com.jwiltenburg.api.controllers.request.UserRequest
 import com.jwiltenburg.api.controllers.request.UserUpdateRequest
 import com.jwiltenburg.api.controllers.response.UserResponse
 import com.jwiltenburg.api.entities.UserEntity
+import com.jwiltenburg.api.exceptions.NotFoundException
 import com.jwiltenburg.api.extensions.toUserEntity
 import com.jwiltenburg.api.extensions.toUserResponse
 import com.jwiltenburg.api.repositories.UserRepository
@@ -11,7 +12,6 @@ import com.jwiltenburg.api.services.UserService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import java.util.Objects.isNull
 
 @Service
 class UserServiceImpl(
@@ -23,8 +23,8 @@ class UserServiceImpl(
 
     override fun getAllUsers(page: Pageable): Page<UserResponse> {
         val pages = userRepository.findAll(page)
-        if(isNull(pages)){
-            throw RuntimeException("Não existe nenhum recurso cadastrado na base de dados.")
+        if(pages.isEmpty){
+            throw NotFoundException(message = "There is no resource registered in the database", errorCode = "U-0001")
         }
         return pages.map { it.toUserResponse() }
     }
@@ -32,38 +32,40 @@ class UserServiceImpl(
     override fun findByNameUser(name: String?): List<UserResponse> {
         val existName = name?.let { userRepository.findByNameContainingIgnoreCase(it) }
         if(existName!!.isEmpty()){
-            throw RuntimeException("Não existe nenhum recurso cadastrado na base de dados.")
+            throw NotFoundException(message = "The name $name does not exist", errorCode = "U-0002")
         }
         return existName.map { it.toUserResponse() }
     }
 
     override fun updateUser(uuid: String, userUpdateRequest: UserUpdateRequest) {
-        val existsByUuid = userRepository.findByUuid(uuid)
-        if(!existsByUuid.isPresent){
-            throw RuntimeException("Não existe nenhum recurso cadastrado na base de dados.")
-        }
+        getByUuid(uuid)
         val id: Long? = this.getById(uuid)
         val entity = userRepository.findById(id!!)
         userRepository.saveAndFlush(userUpdateRequest.toUserEntity(entity.get()))
     }
 
     override fun updatePartUser(uuid: String, userUpdateRequest: UserUpdateRequest) {
-        val existsByUuid = userRepository.findByUuid(uuid)
-        if(!existsByUuid.isPresent){
-            throw RuntimeException("Não existe nenhum recurso cadastrado na base de dados.")
-        }
+        getByUuid(uuid)
         val id: Long? = this.getById(uuid)
         val entity = userRepository.findById(id!!)
         userRepository.saveAndFlush(userUpdateRequest.toUserEntity(entity.get()))
     }
 
     override fun deleteUser(uuid: String) {
+        getByUuid(uuid)
         val id = this.getById(uuid)
         val entity = userRepository.findById(id!!)
         userRepository.deleteById(entity.get().id!!)
     }
 
-    fun getById(uuid: String): Long?{
+    private fun getByUuid(uuid: String) {
+        val existsByUuid = userRepository.findByUuid(uuid)
+        if (!existsByUuid.isPresent) {
+            throw NotFoundException(message = "UUID [$uuid] does not exist", errorCode = "U-0003")
+        }
+    }
+
+    private fun getById(uuid: String): Long?{
         val userEntity = userRepository.findByUuid(uuid)
         return userEntity.get().id
     }
