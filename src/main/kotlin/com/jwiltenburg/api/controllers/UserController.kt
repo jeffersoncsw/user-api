e5e9d479af1b6555c03c1d61920cb520eec69702
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-08-01 20:03:10 -0300

    control refactoring with best practices

--- src/main/kotlin/com/jwiltenburg/api/controllers/UserController.kt
+++ src/main/kotlin/com/jwiltenburg/api/controllers/UserController.kt
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("api/users")
 class UserController(
         val userService: UserService
 ) {
